e3ce8c295ad4815399753f84de1a4c34bbbf6f2c
Jeonhakbong <rainsnow1008@gmail.com>
2022-01-29 20:05:47 +0900

    Update Users Application 220129

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models  # import models( is in same folder as the admin )
 
 # Register your models here.
+@admin.register(
+    models.User
+)  # decorator == admin.site.register(models.User, CustomerUserAdmin)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    # list_display = ("username", "gender", "language", "currency", "superhost")
+    # list_filter = ("superhost",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,46 @@
+from locale import currency
+from pyexpat import model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from config.settings import LANGUAGE_CODE
+
 # Create your models here.
+
+# our customized User class inherit the Abstract class
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    # choices constant variables, list
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),  # (value to go to database, value to show on form)
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+    # add fields you need and then, migrate it
+    # django makes form of it on website
+    # need to set default value of fields or null=True
+    # null option is for database, and blank option is for form on website
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+
+    superhost = models.BooleanField(default=False)
