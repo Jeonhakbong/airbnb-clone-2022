4af657a77c67f213df897964f72bde11d8489631
Jeonhakbong <rainsnow1008@gmail.com>
2022-02-07 18:54:51 +0900

    Create list model and room fieldset 220207

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,85 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "host",
+                    "country",
+                    "city",
+                    "address",
+                )
+            },
+        ),
+        (
+            "Book Info",
+            {
+                "fields": (
+                    "price",
+                    "guests",
+                    "check_in",
+                    "check_out",
+                    "instant_book",
+                )
+            },
+        ),
+        (
+            "Spaces Info",
+            {
+                "fields": (
+                    "room_type",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "city",
+        "country",
+    )
+
+    # we can access to foreign key's attributes
+    search_fields = ["city", "host__username"]
+
+    # filter for many-to-many
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    # self is class, obj(object) is current row on admin panel.
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+
+        return
+
+    count_amenities.short_description = "Num of Amenities"
 
 
 @admin.register(models.Photo)
