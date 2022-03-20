26d592b6645673595a88ed3d41f7d0a104ebe588
Jeonhakbong <90051532+Jeonhakbong@users.noreply.github.com>
2022-03-20 21:51:21 +0900

    Introduction to Views , URLs and Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,17 @@
-from datetime import datetime
 from django.shortcuts import render  # allows us to send response with html inside.
-from django.http import HttpResponse
+from . import models
+
+# from django.http import HttpResponse
 
 
 # view need to return the response
 def all_rooms(request):
-    # print(dir(request))  # we can check the request.
-    # now = datetime.now()
-    # HttpResponse(content=f"<h1>hello, It's {now}.")
-    return render(request, "hello.html")
+    rooms = models.Room.objects.all()
+
+    return render(
+        request,
+        "home/all_rooms.html",
+        context={
+            "rooms": rooms,
+        },
+    )
