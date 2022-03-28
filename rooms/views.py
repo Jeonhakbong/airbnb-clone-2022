eebee34700085df95d684c25d156269911b43acb
Jeonhakbong <90051532+Jeonhakbong@users.noreply.github.com>
2022-03-28 17:15:44 +0900

    create try-exception for pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render  # allows us to send response with html inside.
-from django.core.paginator import Paginator
+from django.shortcuts import (
+    render,
+    redirect,
+)  # allows us to send response with html inside.
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 # from django.http import HttpResponse
@@ -7,20 +10,22 @@ from . import models
 # view need to return the response
 def all_rooms(request):
     # pagination.
-    page = request.GET.get("page")
+    page = int(request.GET.get("page", 1))
     room_list = models.Room.objects.all()  # just only create the queryset.
     # QuerySets are lazy – the act of creating a QuerySet doesn’t involve any database activity.
 
     paginator = Paginator(room_list, per_page=10, orphans=3)
 
-    page = paginator.get_page(page)  # return page object.
-
-    print(vars(page))  # get dictionary in current page.
-    print(vars(page.paginator))
-    return render(
-        request,
-        "home/all_rooms.html",
-        context={  # we can use it in html.
-            "page": page,
-        },
-    )
+    # With page() method, We can control and handle error as we want.
+    try:
+        page = paginator.page(page)  # return page object. (vs. get_page())
+        return render(
+            request,
+            "home/all_rooms.html",
+            context={  # we can use it in html.
+                "page": page,
+            },
+        )
+    except EmptyPage:
+        # page = paginator.page(1)
+        return redirect("/")
