db31abea4e1a2dde6dce172adb278b23d204b058
Jeonhakbong <90051532+Jeonhakbong@users.noreply.github.com>
2022-04-02 23:07:47 +0900

    Handling http404

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
+from django.http import Http404
 from django.utils import timezone
-
-# from django.db.models import
 from django.views.generic import ListView
 from django.urls import reverse
 from django.shortcuts import redirect, render
@@ -29,6 +28,7 @@ def room_detail(request, pk):
     try:
         room = models.Room.objects.get(pk=pk)
         # print(room)
+        return render(request, "rooms/detail.html", context={"room": room})
     except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
-    return render(request, "rooms/detail.html", context={"room": room})
+        # return redirect(reverse("core:home"))
+        raise Http404()  # we can also use http404 status.
