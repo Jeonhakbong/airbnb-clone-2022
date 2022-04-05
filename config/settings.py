484506d73a0699be2db2c6aaaba7b4c5e2933479
Jeonhakbong <rainsnow1008@gmail.com>
2022-04-06 01:23:10 +0900

    Changed room_detail to CBV(RoomDetail)

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3$x_)*)zi%@g5s-eto665zi68&w2jx@rr6uwva$x0ts@4++^=5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,7 @@
 from django.http import Http404
 from django.utils import timezone
-from django.views.generic import ListView
-from django.urls import reverse
-from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 # we don't need to programming like return render. Just configurate it with django.
@@ -24,11 +23,8 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        # print(room)
-        return render(request, "rooms/detail.html", context={"room": room})
-    except models.Room.DoesNotExist:
-        # return redirect(reverse("core:home"))
-        raise Http404()  # we can also use http404 status.
+class RoomDetail(DetailView):
+
+    """RoomDetail Definition"""
+
+    model = models.Room
